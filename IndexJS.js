6c9616ddc1c784cb15b224b2336d5935bba96e4d
Blitz-sys <nuuke32blitz@gmail.com>
2026-08-03 16:36:15 +0530

    Add placeholder page and interactive navigation alerts

---
+++ IndexJS.js
@@ -0,0 +1,12 @@
+const basesButton = document.getElementById("basesButton");
+const armiesButton = document.getElementById("armiesButton");
+const clansButton = document.getElementById("clansButton");
+basesButton.addEventListener("click", () => {
+  alert("Bases Currently empty");
+});
+armiesButton.addEventListener("click", () => {
+  alert("Armies Currently empty");
+});
+clansButton.addEventListener("click", () => {
+  alert("Clans Currently empty");
+});
