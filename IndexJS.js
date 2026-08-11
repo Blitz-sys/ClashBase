b5fdfef72e3f12242a8da337ef694ad7609b5fc1
Blitz-sys <nuuke32blitz@gmail.com>
2026-08-11 17:16:56 +0530

    Fix file name case for bases, armies, clans links

--- IndexJS.js
+++ IndexJS.js
@@ -2,11 +2,11 @@ const basesButton = document.getElementById("basesButton");
 const armiesButton = document.getElementById("armiesButton");
 const clansButton = document.getElementById("clansButton");
 basesButton.addEventListener("click", () => {
-  window.open("Bases.html", "_self");
+  window.open("bases.html", "_self");
 });
 armiesButton.addEventListener("click", () => {
-  window.open("Armies.html", "_self");
+  window.open("armies.html", "_self");
 });
 clansButton.addEventListener("click", () => {
-  window.open("Clans.html", "_self");
+  window.open("clans.html", "_self");
 });
