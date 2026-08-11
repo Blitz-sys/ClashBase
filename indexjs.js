6855537b1280b56e445840cbc06b92a2a46f1608
Blitz-sys <nuuke32blitz@gmail.com>
2026-08-11 22:24:18 +0530

    Use location.href and anchor links for navigation instead of window.open
    Changed button markup and JS to use location.href

--- indexjs.js
+++ indexjs.js
@@ -2,11 +2,11 @@ const basesButton = document.getElementById("basesButton");
 const armiesButton = document.getElementById("armiesButton");
 const clansButton = document.getElementById("clansButton");
 basesButton.addEventListener("click", () => {
-  window.open("bases.html", "_self");
+  window.location.href("bases.html", "_self");
 });
 armiesButton.addEventListener("click", () => {
-  window.open("armies.html", "_self");
+  window.location.href("armies.html", "_self");
 });
 clansButton.addEventListener("click", () => {
-  window.open("clans.html", "_self");
+  window.location.href("clans.html", "_self");
 });
