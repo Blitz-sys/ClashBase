9ba6367430f15066958bebea1a722179662e152f
Blitz-sys <nuuke32blitz@gmail.com>
2026-08-18 12:33:30 +0530

    Use `window.location.assign` instead of `window.location.href`

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const armiesButton = document.getElementById("armiesButton");
 const clansButton = document.getElementById("clansButton");
 const darkModeButton = document.getElementById("darkModeButton");
 basesButton.addEventListener("click", () => {
-  window.location.href("bases.html", "_self");
+  window.location.assign("bases.html", "_self");
 });
 armiesButton.addEventListener("click", () => {
-  window.location.href("armies.html", "_self");
+  window.location.assign("armies.html", "_self");
 });
 clansButton.addEventListener("click", () => {
-  window.location.href("clans.html", "_self");
+  window.location.assign("clans.html", "_self");
 });
 darkModeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
