18bb54ef7800364d8e8664d4570b50af6038ac86
Blitz-sys <nuuke32blitz@gmail.com>
2026-08-18 20:20:20 +0530

    Update dark mode state handling to use boolean

--- localStorage.js
+++ localStorage.js
@@ -1,18 +1,14 @@
 const darkModeButton = document.getElementById("darkModeButton");
 const outerBody = document.querySelector(".outer-body");
 const startupButtons = document.querySelectorAll(".button-container-start button");
-let darkModeState = localStorage.getItem("darkMode") || "disabled";
+let darkModeState = localStorage.getItem("darkMode") === "true";
 function toggleDarkMode() {
-  if (darkModeState === "disabled") {
-    darkModeState = "enabled";
-  } else {
-    darkModeState = "disabled";
-  }
+  darkModeState = !darkModeState
   localStorage.setItem("darkMode", darkModeState);
   applyDarkMode();
 }
 function applyDarkMode() {
-  if (darkModeState === "enabled") {
+  if (darkModeState) {
     outerBody.style.backgroundColor = "#0c0105";
     outerBody.style.color = "ghostwhite";
     startupButtons.forEach(button => {
